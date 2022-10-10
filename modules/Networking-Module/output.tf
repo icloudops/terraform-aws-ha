46ce17c23c25c6c81b74fa31a96c770e172b6a69
Belal Hassan <belal.abuelella@outlook.com>
2022-10-10 12:07:38 +0200

    add subnets id in output file

--- modules/Networking-Module/output.tf
+++ modules/Networking-Module/output.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
-value = aws_vpc.vpc.id
+    value = aws_vpc.vpc.id
+}
+output "public-subnet-1a_id" {
+    value = aws_subnet.public-subnet-1a.id
+}
+output "public-subnet-1b_id" {
+    value = aws_subnet.public-subnet-1b.id
+}
+output "private-subnet-1a_id" {
+    value = aws_subnet.private-subnet-1a.id
+}
+output "private-subnet-1b_id" {
+    value = aws_subnet.private-subnet-1b.id
 }
